c7c64e4db36d252c8712cf1adb1026394734409c
Ivan Torres Garcia <ivantorresgarcia.02@gmail.com>
2022-07-16 19:44:43 +0200

    build(#2): integrate MUI (Material Design) in UI

---
+++ ui/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button'
+
+const Home: NextPage = () => {
+  return <Button variant="contained">Hello World</Button>
+}
+
+export default Home
