4c5ddfa8251e9def735fbe3371ce871ae2d8e638
Iván Torres García <ivntorresgarcia.02@gmail.com>
2022-10-08 16:06:04 +0200

    build(migrations): create migrations (#23)

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +23,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::factory(10)
+        ->sequence(fn ($sequence) => ['name' => 'Product '.$sequence->index + 1])
+        ->create();
     }
 }
