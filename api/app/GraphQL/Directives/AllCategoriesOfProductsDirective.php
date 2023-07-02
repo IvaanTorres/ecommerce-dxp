4db3f2035623c043c962f9d8f74150c0ec38fd48
IvaanTorres <ivantorresgarcia.02@gmail.com>
2023-07-02 11:25:43 +0200

    feat(brands): get all categories of products that belong to a brand (#32)

---
+++ api/app/GraphQL/Directives/AllCategoriesOfProductsDirective.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\GraphQL\Directives;
+
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+use Nuwave\Lighthouse\Schema\Values\FieldValue;
+use Nuwave\Lighthouse\Support\Contracts\FieldResolver;
+
+final class AllCategoriesOfProductsDirective extends BaseDirective implements FieldResolver
+{
+    // TODO implement the directive https://lighthouse-php.com/master/custom-directives/getting-started.html
+
+    public static function definition(): string
+    {
+        return /** @lang GraphQL */ <<<'GRAPHQL'
+directive @allCategoriesOfProducts on FIELD_DEFINITION
+GRAPHQL;
+    }
+
+    /**
+     * Set a field resolver on the FieldValue.
+     *
+     * This must call $fieldValue->setResolver() before returning
+     * the FieldValue.
+     *
+     * @param  \Nuwave\Lighthouse\Schema\Values\FieldValue  $fieldValue
+     * @return \Nuwave\Lighthouse\Schema\Values\FieldValue
+     */
+    public function resolveField(FieldValue $fieldValue)
+    {
+        $fieldValue->setResolver(function ($root, array $args, $context, $info) {
+            // Get the products
+            $products = $root->products;
+
+            // Get the categories of the products
+            $categories = $products->map(function ($product) {
+                return $product->categories;
+            })->flatten();
+
+            // Return the unique categories by id
+            $categories = $categories->unique('id');
+
+            return $categories;
+        });
+
+        return $fieldValue;
+    }
+}
