9ea96a2422ae8e1a1d825102092455275bb5a863
IvaanTorres <ivantorresgarcia.02@gmail.com>
2023-07-06 21:02:12 +0200

    build(nodejs-nextjs): upgrade nodejs version from 16 to 18 & nextjs 12 to 13 (#33)

--- ui/src/lib/client.js
+++ ui/src/lib/client.js
@@ -1,12 +1,13 @@
 /* eslint-disable import/no-unresolved */
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
+import env from '../shared/enums/config/environment'
 
 //* This client is used for server-side rendering
 export const { getClient } = registerApolloClient(() => new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     // Since we're using SSR, we need to provide the full URL (localhost can't be resolved)
-    uri: 'http://127.0.0.1:8000/graphql',
+    uri: env.NEXT_PUBLIC_API_URL,
   }),
 }))
